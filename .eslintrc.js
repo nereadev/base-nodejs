c88640195a27d2fca8408e157fe1bbe8dd5724e2
Nerea García <nereagarcia.c@gmail.com>
2021-03-31 11:39:23 +0200

    fix rules eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,35 @@ module.exports = {
     node: true,
   },
   extends: [
-    'airbnb-base',
+    "airbnb-base",
   ],
   parserOptions: {
     ecmaVersion: 12,
   },
   rules: {
-  },
+    quotes: [
+      "error",
+      "double"
+    ],
+    semi: "error",
+    "no-console": "off",
+    "comma-dangle": "off",
+    "arrow-parens": "off",
+    "no-unused-vars": "off",
+    "no-extra-boolean-cast": "off",
+    "no-else-return": "off",
+    "no-plusplus": "off",
+    "no-debugger": "off",
+    "prefer-destructuring": "off",
+    "no-use-before-define": "off",
+    "no-restricted-syntax": "off",
+    "no-shadow": "off",
+    "no-underscore-dangle": "off",
+    "func-names": "off",
+    "max-len": "off",
+    "consistent-return": "off",
+    "space-before-function-paren": "off",
+    "no-param-reassign": "off",
+    "no-restricted-globals": "off"
+  }
 };
